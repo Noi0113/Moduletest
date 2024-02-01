e79bcfd7588d6df9f467d9f5676ea3cc1751c3a6
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-01 18:26:22 +0900

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -47,7 +47,7 @@ def app():
     conn.close()
 
     # Gitリポジトリのパスを指定
-    repo = Repo(r'C:\Users\81907\Moduletest')
+    repo = Repo('C:/Users/81907/Moduletest')
 
     # 変更をステージング
     repo.git.add('test-monketsu.db')
