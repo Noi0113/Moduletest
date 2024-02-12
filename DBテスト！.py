5567a0dbcb1d24781163e93bee0a0ad11c3fc206
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-12 18:58:09 +0900

    Update DBテスト！.py

--- DBテスト！.py
+++ DBテスト！.py
@@ -32,7 +32,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS your_table_name (
 
 # Streamlitアプリケーション
 def main():
-    st.title('データ入力、アクセストークンをどうにかした!!')
+    st.title('データ入力、アクセストークンをどうにかした(パスワードをトークンに変更)')
 
     # データ入力フォーム
     input_data1 = st.text_input('データ1')
