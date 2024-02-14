d5cadb48f45cdc1c3cc22ce338b8c41586197ff8
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-15 02:09:45 +0900

    Update スプシ接続.py

--- スプシ接続.py
+++ スプシ接続.py
@@ -2,11 +2,13 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 # Streamlitアプリケーション
-st.title('Streamlit Google Sheets Connection！！！！！')
+st.title('Streamlit Google Sheets Connection！')
 
+# スコープの設定
+scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
 # Google Sheets認証情報の読み込み
-credentials = ServiceAccountCredentials.from_json_keyfile_name('monketsu2-59ce80be021b.json', ['https://spreadsheets.google.com/feeds'])
+credentials = ServiceAccountCredentials.from_json_keyfile_name('monketsu2-2b83cfd57ed6.json', ['https://spreadsheets.google.com/feeds'])
 gc = gspread.authorize(credentials)
 
 # Google Sheetsのシートを開く
