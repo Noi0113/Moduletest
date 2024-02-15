e8428a1a000ffccd5117b84ea3f1209e0ed44678
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-15 19:04:01 +0900

    Update スプシ接続.py

--- スプシ接続.py
+++ スプシ接続.py
@@ -16,16 +16,26 @@ gc = gspread.authorize(credentials)
 # Google Sheetsのシート2を開く
 sheet = gc.open('monketsu-karuta').get_worksheet(1)
 
+absent_options = ['第1試合','第2試合','第3試合','第4試合']
+
 # Streamlitでの入力
 user_input = st.text_input('Enter your information:')
 input_level = st.selectbox('級を入力してください(必須)',options=['A','B','C','D','E'])
+absent_matches = st.multiselect('欠席する試合を入力してください(複数選択可)', absent_options)
 
+#休む試合は複数選択のため、リスト化
+st.write(absent_matches)
+
+#提出ボタンを押してデータを格納
 if st.button('Submit'):
     column_number = 1
     column_number2 = 2  # 例として2列目に書き込む
     last_row = len(sheet.col_values(column_number2)) + 1
     sheet.update_cell(last_row, column_number, user_input)
     sheet.update_cell(last_row, column_number2, input_level)
+    #sheet.update_cell(last_row, 3, absent_list[0])
+    #sheet.update_cell(last_row, 4, absent_list[0])
+    
     st.success('Data has been written to Google Sheets!')
 
 
